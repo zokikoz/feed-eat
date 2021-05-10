99515eb0f3747b351cbb4e53efad102900d8c9c8
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-11 00:39:18 +0800

    Generated Items controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'items#index'
+
+  resources :items, only: [:index, :show]
   resources :channels
+
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 end
