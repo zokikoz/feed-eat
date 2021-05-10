79fbecd74166e722cf0e4beb248a7b5781c4da5b
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-11 02:32:48 +0800

    Channel model validation and testing

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,7 @@
 class Channel < ApplicationRecord
+  URL_REGEXP = %r{(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix
+  validates :link, presence: true, format: { with: URL_REGEXP, message: 'You provided invalid URL' }
+
   has_many :items, dependent: :destroy
 
   after_create :rss_worker_start
--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Channel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without a link" do
+    channel = Channel.new(link: nil)
+    expect(channel).to_not be_valid
+  end
+
+  it "is not valid with bad URL" do
+    channel = Channel.new(link: 'http://')
+    expect(channel).to_not be_valid
+  end
+
+  it "is valid with good URL" do
+    channel = Channel.new(link: 'https://www.ruby-lang.org/en/feeds/news.rss')
+    expect(channel).to be_valid
+  end
 end
