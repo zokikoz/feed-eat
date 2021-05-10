30e8aae6a539766c7d963c00b94ee92f49a66c9d
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-11 01:11:06 +0800

    Starting RSS worker job after creating or updating channels

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,10 @@
 class Channel < ApplicationRecord
   has_many :items, dependent: :destroy
+
+  after_create :rss_worker_start
+  after_update :rss_worker_start
+
+  def rss_worker_start
+    RssWorker.perform_async
+  end
 end
