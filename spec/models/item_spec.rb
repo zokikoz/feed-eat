1ff471485c042547e13e935bd8de90d9354e9362
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-11 12:05:32 +0800

    Item model associations test. DB seeding

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(link: nil) }
+
+  describe "Associations" do
+    it { should belong_to(:channel) }
+  end
 end
