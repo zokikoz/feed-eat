08611c51bf51de45b53b7516ad1f3bd7435ab86e
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-11 14:47:23 +0800

    Added RSS worker test

--- spec/workers/rss_worker_spec.rb
+++ spec/workers/rss_worker_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.inline!
+
 RSpec.describe RssWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    Channel.delete_all
+    Channel.create!(link: 'https://www.ruby-lang.org/en/feeds/news.rss')
+  end
+  it "is load entries to items table" do
+    Item.delete_all
+    expect { RssWorker.perform_async }.to change { Item.count }.from(0)
+  end
 end
