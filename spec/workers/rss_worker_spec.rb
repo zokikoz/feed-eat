08f01057722f8574829c277925319267948c1cf1
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-12 22:13:56 +0800

    Using HTTP response stub for tests

--- spec/workers/rss_worker_spec.rb
+++ spec/workers/rss_worker_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 require 'sidekiq/testing'
+require 'webmock/rspec'
+
 Sidekiq::Testing.inline!
 
 RSpec.describe RssWorker, type: :worker do
   before(:each) do
+    rss_response_file = File.new("#{::Rails.root}/tmp/rss_response.txt")
+    stub_request(:get, "https://www.ruby-lang.org/en/feeds/news.rss")
+      .to_return(status: 200, body: rss_response_file, headers: {})
+
     Channel.delete_all
     Channel.create!(link: 'https://www.ruby-lang.org/en/feeds/news.rss')
   end
