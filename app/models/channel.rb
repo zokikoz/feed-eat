45f75f6e4bd494ac75e2ff4504214bc7adfafdfa
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-13 22:14:53 +0800

    Added uniqueness validation

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,17 +1,19 @@
 class Channel < ApplicationRecord
   # URL_REGEXP = %r{(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix
   # URL_REGEXP = %r{\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z}i
-  validates :link, presence: true
+  validates :link, presence: true, uniqueness: true
   validate :active_feed
 
   has_many :items, dependent: :destroy
 
   after_create :rss_worker_start
 
+  # Starting RSS worker
   def rss_worker_start
     RssWorker.perform_async
   end
 
+  # Checking feed
   def active_feed
     feed_url = URI.parse(link)
     RSS::Parser.parse(feed_url)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :channel
+  validates :guid, uniqueness: true
 end
