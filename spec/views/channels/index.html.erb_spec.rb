d478dd8c1d234d4dfa7cc5507ce7e61dc4155c1e
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-13 09:58:20 +0800

    More stubbing RSS worker

--- spec/views/channels/index.html.erb_spec.rb
+++ spec/views/channels/index.html.erb_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "channels/index", type: :view do
   before(:each) do
+    # Stub for the callback method triggering RSS worker
+    allow_any_instance_of(Channel).to receive(:rss_worker_start).and_return(true)
+
     Channel.delete_all
-    assign(:channels, [
-      Channel.create!(
-        link: "https://www.ruby-lang.org/en/feeds/news.rss"
-      ),
-      Channel.create!(
-        link: "https://www.ruby-lang.org/en/feeds/news.rss"
-      )
-    ])
+    assign(:channels, [Channel.create!(link: "https://www.ruby-lang.org/en/feeds/news.rss"),
+                       Channel.create!(link: "https://www.ruby-lang.org/en/feeds/news.rss")])
   end
 
   it "renders a list of channels" do
