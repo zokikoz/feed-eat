42be8cb9ab1421867a7c4ad36bedbae1d6a86c48
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-13 18:39:19 +0800

    Added RSS feed validation

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,14 +1,24 @@
 class Channel < ApplicationRecord
   # URL_REGEXP = %r{(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix
   # URL_REGEXP = %r{\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z}i
-  validates :link, presence: true, url: true
+  validates :link, presence: true
+  validate :active_feed
 
   has_many :items, dependent: :destroy
 
   after_create :rss_worker_start
-  after_update :rss_worker_start
 
   def rss_worker_start
     RssWorker.perform_async
+  end
+
+  def active_feed
+    feed_url = URI.parse(link)
+    RSS::Parser.parse(feed_url)
+  rescue RSS::Error
+    errors.add(:link, 'is invalid RSS format')
+  rescue StandardError => e
+    logger.error "RSS link error: #{e.message}"
+    errors.add(:link, 'can\'t be connected')
   end
 end
