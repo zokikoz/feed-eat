f35f14d97f8a6fd1357ae365312b6305363f6296
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-18 16:16:32 +0800

    Updated RSS worker

--- app/workers/rss_worker.rb
+++ app/workers/rss_worker.rb
@@ -17,7 +17,7 @@ class RssWorker
     feed_url = URI.parse(link)
     RSS::Parser.parse(feed_url)
   rescue StandardError => e
-    logger.error "RSS link error: #{e.message}"
+    logger.error "Feed connection error: #{e.message}"
   end
 
   # Getting entries from feed
@@ -39,24 +39,29 @@ class RssWorker
   end
 
   def rss_select(item)
-    { guid: item.guid.content, title: item.title, description: item.description, link: item.link, pub_date: item.pubDate.rfc2822 }
-  rescue NoMethodError
-    logger.error "Item field is missing"
-    { guid: nil }
+    item_hash = item.as_json
+    { guid: item_hash.dig('guid', 'content'),
+      title: item_hash['title'],
+      description: item_hash['description'],
+      link: item_hash['link'],
+      pub_date: item_hash['pubDate'] }
   end
 
   def atom_select(item)
-    { guid: item.id.content, title: item.title.content, description: item.content.content, link: item.link.href, pub_date: item.updated.content.rfc2822 }
-  rescue NoMethodError
-    logger.error "Item field is missing"
-    { guid: nil }
+    item_hash = item.as_json
+    { guid: item_hash.dig('id', 'content'),
+      title: item_hash.dig('title', 'content'),
+      description: item_hash.dig('content', 'content'),
+      link: item_hash.dig('link', 'href'),
+      pub_date: item_hash.dig('updated', 'content') }
   end
 
   # Updating items table
   def items_update(selected_items, channel)
     selected_items.each do |item|
-      next if item[:guid].blank?
+      next if item[:guid].nil? || item[:pub_date].nil?
 
+      item[:pub_date] = item[:pub_date].to_datetime
       updated_item = channel.items.where(guid: item[:guid]).first_or_initialize
       updated_item.update(title: item[:title], description: item[:description], link: item[:link], pub_date: item[:pub_date])
     end
