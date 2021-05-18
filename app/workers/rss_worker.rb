e658fb94e427e185131278e50d4eae31c8cf37e9
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-18 22:41:39 +0800

    Simple date correction

--- app/workers/rss_worker.rb
+++ app/workers/rss_worker.rb
@@ -5,7 +5,10 @@ class RssWorker
     Channel.all.each do |channel|
       feed = feed_connect(channel.link)
       selected_items = feed_select(feed)
-      items_update(selected_items, channel)
+      selected_items.each do |item|
+        item[:pub_date] = date_correct(item[:pub_date])
+        item_update(item, channel)
+      end
     end
   end
 
@@ -38,6 +41,7 @@ class RssWorker
     selected_items
   end
 
+  # RSS items parser
   def rss_select(item)
     { guid: item.try(:guid).try(:content),
       title: item.title,
@@ -46,6 +50,7 @@ class RssWorker
       pub_date: item.pubDate }
   end
 
+  # Atom entries parser
   def atom_select(item)
     { guid: item.try(:id).try(:content),
       title: item.try(:title).try(:content),
@@ -55,14 +60,21 @@ class RssWorker
   end
 
   # Updating items table
-  def items_update(selected_items, channel)
-    selected_items.each do |item|
-      next if item[:guid].blank? || item[:pub_date].blank? 
+  def item_update(item, channel)
+    return if item[:guid].blank? || item[:pub_date].blank?
 
-      updated_item = channel.items.where(guid: item[:guid]).first_or_initialize
-      updated_item.update(title: item[:title], description: item[:description], link: item[:link], pub_date: item[:pub_date])
-    end
+    updated_item = channel.items.where(guid: item[:guid]).first_or_initialize
+    updated_item.update(title: item[:title], description: item[:description], link: item[:link], pub_date: item[:pub_date])
   rescue StandardError => e
     logger.error "Update error: #{e.message}"
+  end
+
+  # Simple date correction
+  def date_correct(pub_date)
+    return if pub_date.blank?
+
+    pub_date.to_datetime
+  rescue Date::Error
+    nil
   end
 end
