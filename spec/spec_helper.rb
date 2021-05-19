94bd69d25be2190f99643508770ae4b81a06dd56
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-05-19 12:36:59 +0800

    Modified simplecov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,9 +16,8 @@
 
 require 'simplecov'
 SimpleCov.start 'rails' do
-  add_filter '/bin/'
-  add_filter '/db/'
-  add_filter '/spec/'
+  add_filter '/app/channels/'
+  add_filter '/app/mailers/'
 end
 
 RSpec.configure do |config|
